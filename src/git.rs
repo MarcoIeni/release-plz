ffa3ffed4a2f39a99c952da15a2c89a2c3aaf0d7
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2021-12-25 12:01:59 +0100

    wip

--- src/git.rs
+++ src/git.rs
@@ -10,13 +10,23 @@ use anyhow::Context;
 pub struct Repo {
     /// Repository root directory
     directory: PathBuf,
+    current_branch: String,
 }
 
 impl Repo {
-    pub fn new(directory: impl AsRef<Path>) -> Self {
-        Self {
+    pub fn new(directory: impl AsRef<Path>) -> anyhow::Result<Self> {
+        let current_branch =
+            Self::git_in_dir(directory.as_ref(), &["rev-parse", "--abbrev-ref", "HEAD"])?;
+        let current_branch = stdout(current_branch)?;
+
+        Ok(Self {
             directory: directory.as_ref().to_path_buf(),
-        }
+            current_branch,
+        })
+    }
+
+    fn current_branch(&self) -> &str {
+        &self.current_branch
     }
 
     fn current_commit(&self) -> anyhow::Result<String> {
@@ -30,21 +40,21 @@ impl Repo {
     fn nth_commit(&self, nth: usize) -> anyhow::Result<String> {
         let nth = nth.to_string();
         let output = self.git(&["--format=\"%H\"", "-n", &nth])?;
-        let last_two_commits = String::from_utf8(output.stdout)?;
-        let previous_commit = last_two_commits
+        let commit_list = stdout(output)?;
+        let previous_commit = commit_list
             .lines()
             .last()
             .context("repository has no commits")?;
         Ok(previous_commit.to_string())
     }
 
+    fn git_in_dir(dir: &Path, args: &[&str]) -> io::Result<Output> {
+        Command::new("git").arg("-C").arg(dir).args(args).output()
+    }
+
     /// Run a git command in the repository git directory
     fn git(&self, args: &[&str]) -> io::Result<Output> {
-        Command::new("git")
-            .arg("-C")
-            .arg(&self.directory)
-            .args(args)
-            .output()
+        Self::git_in_dir(&self.directory, args)
     }
 
     /// Checkout to the latest commit. I.e. go back in history of 1 commit.
@@ -58,22 +68,41 @@ impl Repo {
     pub fn edited_file_in_current_commit(&self) -> anyhow::Result<Vec<PathBuf>> {
         let commit = &self.current_commit()?;
         let output = self.git(&["diff-tree", "--no-commit-id", "--name-only", "-r", commit])?;
-        let files = String::from_utf8(output.stdout)?;
+        let files = stdout(output)?;
         let files: Result<Vec<PathBuf>, io::Error> = files.lines().map(fs::canonicalize).collect();
         Ok(files?)
+    }
+
+    fn previous_commit_at_path(&self, path: impl AsRef<str>) -> anyhow::Result<String> {
+        self.nth_commit_at_path(2, path)
+    }
+
+    fn nth_commit_at_path(&self, nth: usize, path: impl AsRef<str>) -> anyhow::Result<String> {
+        let nth = nth.to_string();
+        let output = self.git(&["log", "-p", path.as_ref(), "--format=\"%H\"", "-n", &nth])?;
+        let commit_list = stdout(output)?;
+        let previous_commit = commit_list
+            .lines()
+            .last()
+            .context("repository has no commits")?;
+        Ok(previous_commit.to_string())
     }
 
     /// Return the list of edited files of that commit. Absolute Path.
     fn edited_file(&self, commit: &str) -> anyhow::Result<Vec<PathBuf>> {
         let output = self.git(&["diff-tree", "--no-commit-id", "--name-only", "-r", commit])?;
-        let files = String::from_utf8(output.stdout)?;
+        let files = stdout(output)?;
         let files: Result<Vec<PathBuf>, io::Error> = files.lines().map(fs::canonicalize).collect();
         Ok(files?)
     }
 
     pub fn get_current_commit_message(&self) -> anyhow::Result<String> {
         let output = self.git(&["log", " -1", "--pretty=format:%s"])?;
-        let message = String::from_utf8(output.stdout)?;
-        Ok(message)
+        stdout(output)
     }
+}
+
+fn stdout(output: Output) -> anyhow::Result<String> {
+    let stdout = String::from_utf8(output.stdout)?;
+    Ok(stdout)
 }
