168052f1255076cc297affc5e9305f57cac2ec8c
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2021-12-25 23:57:05 +0100

    version bump

--- src/version.rs
+++ src/version.rs
@@ -1,5 +1,6 @@
 use cargo_edit::VersionExt;
 use cargo_metadata::Version;
+use conventional_commit_parser::commit::{CommitType, ConventionalCommit};
 
 use crate::Diff;
 
@@ -14,9 +15,59 @@ impl NextVersion for Version {
         if !diff.remote_crate_exists {
             self
         } else {
-            self.increment_patch();
-            self
+            let increment = get_increment_from_commits(&self, &diff.commits);
+            increment.bump(&self)
         }
+    }
+}
+
+fn get_increment_from_commits(current_version: &Version, commits: &[String]) -> VersionIncrement {
+    let commits: Vec<ConventionalCommit> = commits
+        .iter()
+        .filter_map(|c| conventional_commit_parser::parse(c).ok())
+        .collect();
+
+    version_increment_from_commit_history(current_version, &commits)
+}
+
+// taken from [cocogitto](https://github.com/cocogitto/cocogitto/blob/3a3249cd0167adc5183f2a384155d5e1120a500d/src/conventional/version.rs#L44)
+fn version_increment_from_commit_history(
+    current_version: &Version,
+    commits: &[ConventionalCommit],
+) -> VersionIncrement {
+    let is_major_bump =
+        || current_version.major != 0 && commits.iter().any(|commit| commit.is_breaking_change);
+
+    let is_minor_bump = || {
+        commits
+            .iter()
+            .any(|commit| commit.commit_type == CommitType::Feature)
+    };
+
+    if is_major_bump() {
+        VersionIncrement::Major
+    } else if is_minor_bump() {
+        VersionIncrement::Minor
+    } else {
+        VersionIncrement::Patch
+    }
+}
+
+enum VersionIncrement {
+    Major,
+    Minor,
+    Patch,
+}
+
+impl VersionIncrement {
+    fn bump(&self, version: &Version) -> Version {
+        let mut new_version = version.clone();
+        match self {
+            VersionIncrement::Major => new_version.increment_major(),
+            VersionIncrement::Minor => new_version.increment_minor(),
+            VersionIncrement::Patch => new_version.increment_patch(),
+        }
+        new_version
     }
 }
 
