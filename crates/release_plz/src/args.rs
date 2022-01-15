ceeacc5f2c29e502a8ecb52f7b57a4e9f7df8cbb
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-01-15 23:24:03 +0100

    add docs

--- crates/release_plz/src/args.rs
+++ crates/release_plz/src/args.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use anyhow::anyhow;
 use release_plz::GitHub;
 use secrecy::SecretString;
@@ -12,16 +14,29 @@ pub struct CliArgs {
 
 #[derive(clap::Subcommand, Debug)]
 pub enum Command {
-    Update,
+    /// Update crates version based on commit messages.
+    Update(Update),
+    /// Update crates version based on commit messages and create a Pull Request.
     UpdateWithPr(UpdateWithPr),
 }
 
 #[derive(clap::Parser, Debug)]
+pub struct Update {
+    /// Path to the current released version of the project you want to update.
+    /// If not provided, the crates of your project will be compared with the
+    /// ones published in the cargo registry (only crates.io at the moment).
+    /// Normally, this parameter is used only if the published version of
+    /// your project is already available locally.
+    /// For example, it could be the path to the project with a `git checkout` on its latest tag.
+    reference_project: Option<PathBuf>,
+}
+
+#[derive(clap::Parser, Debug)]
 pub struct UpdateWithPr {
-    /// GitHub token
+    /// GitHub token used to create the pull request.
     #[clap(long)]
     pub github_token: SecretString,
-    /// GitHub repository url
+    /// GitHub repository url where your project is hosted.
     #[clap(long)]
     pub repo_url: Url,
 }
