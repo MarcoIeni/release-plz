db79994e5b066242fcfcd18c1dcef199afbcb916
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-01-31 22:47:38 +0100

    use package word

--- crates/release_plz_core/src/update.rs
+++ crates/release_plz_core/src/update.rs
@@ -64,7 +64,7 @@ impl Project {
         };
         debug!("project_root: {root:?}");
         Ok(Self {
-            packages: list_crates(manifest)?,
+            packages: list_packages(manifest)?,
             root,
             manifest_dir,
         })
@@ -97,25 +97,26 @@ impl Project {
 #[instrument]
 pub fn next_versions(input: &UpdateRequest) -> anyhow::Result<(Vec<(Package, Version)>, Repo)> {
     let local_project = Project::new(&input.local_manifest)?;
-    let remote_crates = get_remote_crates(input.remote_manifest.as_ref(), &local_project.packages)?;
+    let remote_packages =
+        get_remote_packages(input.remote_manifest.as_ref(), &local_project.packages)?;
 
     // copy the repository into a temporary directory, so that we are not sure we don't alter the original one
     let tmp_project_root = tempdir().context("cannot create temporary directory")?;
     let repository = local_project.get_repo(tmp_project_root.as_ref())?;
 
     debug!("calculating local packages");
-    let crates_to_update = packages_to_update(local_project, &remote_crates, &repository)?;
-    debug!("crates to update: {:?}", &crates_to_update);
-    Ok((crates_to_update, repository))
+    let packages_to_update = packages_to_update(local_project, &remote_packages, &repository)?;
+    debug!("packages to update: {:?}", &packages_to_update);
+    Ok((packages_to_update, repository))
 }
 
 /// Update a local rust project
 #[instrument]
 pub fn update(input: &UpdateRequest) -> anyhow::Result<(Vec<(Package, Version)>, Repo)> {
-    let (crates_to_update, repository) = next_versions(input)?;
+    let (packages_to_update, repository) = next_versions(input)?;
 
-    update_versions(&crates_to_update);
-    Ok((crates_to_update, repository))
+    update_versions(&packages_to_update);
+    Ok((packages_to_update, repository))
 }
 
 #[instrument(
@@ -124,7 +125,7 @@ pub fn update(input: &UpdateRequest) -> anyhow::Result<(Vec<(Package, Version)>,
 )]
 fn get_diff(
     package: &Package,
-    remote_crates: &BTreeMap<String, Package>,
+    remote_packages: &BTreeMap<String, Package>,
     repository: &Repo,
     project_root: &Path,
 ) -> anyhow::Result<Diff> {
@@ -133,18 +134,18 @@ fn get_diff(
         repository.directory().join(relative_path)
     };
     repository.checkout_head()?;
-    let remote_crate = remote_crates.get(&package.name);
-    let mut diff = Diff::new(remote_crate.is_some());
+    let remote_package = remote_packages.get(&package.name);
+    let mut diff = Diff::new(remote_package.is_some());
     if let Err(_err) = repository.checkout_last_commit_at_path(&package_path) {
         // there are no commits for this package
         return Ok(diff);
     }
     loop {
         let current_commit_message = repository.current_commit_message()?;
-        if let Some(remote_crate) = remote_crate {
-            debug!("remote crate {} found", remote_crate.name);
+        if let Some(remote_package) = remote_package {
+            debug!("remote package {} found", remote_package.name);
             let are_packages_equal = {
-                let remote_path = remote_crate
+                let remote_path = remote_package
                     .manifest_path
                     .parent()
                     .context("cannot find parent directory")?;
@@ -152,16 +153,16 @@ fn get_diff(
             };
             if are_packages_equal {
                 debug!("packages are equal");
-                // The local crate is identical to the remote one, which means that
-                // the crate was published at this commit, so we will not count this commit
+                // The local package is identical to the remote one, which means that
+                // the package was published at this commit, so we will not count this commit
                 // as part of the release.
                 // We can process the next create.
                 break;
-            } else if remote_crate.version != package.version {
+            } else if remote_package.version != package.version {
                 debug!("the local package {} has already a different version with respect to the remote package, so release-plz will not update it", package.name);
                 break;
             } else {
-                debug!("crates are different");
+                debug!("packages are different");
                 // At this point of the git history, the two crates are different,
                 // which means that this commit is not present in the published package.
                 diff.commits.push(current_commit_message.clone());
@@ -217,12 +218,12 @@ impl CratePath for Package {
 }
 
 /// Return [`BTreeMap`] with "package name" as key
-fn get_remote_crates(
+fn get_remote_packages(
     remote_manifest: Option<&PathBuf>,
     local_crates: &[Package],
 ) -> anyhow::Result<BTreeMap<String, Package>> {
     let remote_crates = match remote_manifest {
-        Some(manifest) => list_crates(manifest)?,
+        Some(manifest) => list_packages(manifest)?,
         None => {
             let local_crates_names: Vec<&str> =
                 local_crates.iter().map(|c| c.name.as_str()).collect();
@@ -256,7 +257,7 @@ fn set_version(package_path: &Path, version: &Version) {
     local_manifest.write().expect("cannot update manifest");
 }
 
-fn list_crates(directory: &Path) -> anyhow::Result<Vec<Package>> {
+fn list_packages(directory: &Path) -> anyhow::Result<Vec<Package>> {
     cargo_edit::workspace_members(Some(directory))
         .map_err(|e| anyhow!("cannot read workspace members: {e}"))
 }
