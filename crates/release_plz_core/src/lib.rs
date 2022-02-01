dae72ef60de5108d9678e410408169c5caab052f
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-02-01 22:22:08 +0100

    split release_pr file

--- crates/release_plz_core/src/lib.rs
+++ crates/release_plz_core/src/lib.rs
@@ -1,20 +1,15 @@
 mod download;
+mod release_pr;
 mod update;
 mod version;
 
+pub use release_pr::*;
 pub use update::*;
 
-use git_cmd::Repo;
-
-use anyhow::Context;
-use fake::Fake;
-use octocrab::OctocrabBuilder;
-use secrecy::{ExposeSecret, SecretString};
-use tracing::{instrument, Span};
-
-/// Difference between local and remote package
+/// Difference between local and remote package (i.e. the last released version)
 #[derive(Debug)]
 struct Diff {
+    /// List of commits from last released vesion to last local changes
     pub commits: Vec<String>,
     /// Whether the package name exists in the remote package or not
     pub remote_package_exists: bool,
@@ -31,72 +26,4 @@ impl Diff {
     fn should_update_version(&self) -> bool {
         self.remote_package_exists && !self.commits.is_empty()
     }
-}
-
-#[derive(Debug)]
-pub struct ReleasePrRequest {
-    pub github: GitHub,
-    pub update_request: UpdateRequest,
-}
-
-#[derive(Debug)]
-pub struct GitHub {
-    pub owner: String,
-    pub repo: String,
-    pub token: SecretString,
-}
-
-/// Update a local rust project and raise a pull request
-#[instrument]
-pub async fn release_pr(input: &ReleasePrRequest) -> anyhow::Result<()> {
-    let (packages_to_update, repository) = next_versions(&input.update_request)?;
-    if !packages_to_update.is_empty() {
-        // TODO think about better naming
-        let random_number: u64 = (100_000_000..999_999_999).fake();
-        let release_branch = format!("release-{}", random_number);
-        create_release_branch(&repository, &release_branch)?;
-        open_pr(&release_branch, &input.github).await?;
-    }
-
-    Ok(())
-}
-
-#[instrument(
-    fields(
-        default_branch = tracing::field::Empty,
-    )
-)]
-async fn open_pr(release_branch: &str, github: &GitHub) -> anyhow::Result<()> {
-    let client = OctocrabBuilder::new()
-        .personal_token(github.token.expose_secret().clone())
-        .build()
-        .context("Failed to build GitHub client")?;
-
-    let default_branch = client
-        .repos(&github.owner, &github.repo)
-        .get()
-        .await
-        .context(format!(
-            "failed to retrieve GitHub repository {}/{}",
-            github.owner, github.repo
-        ))?
-        .default_branch
-        .context("failed to retrieve default branch")?;
-    Span::current().record("default_branch", &default_branch.as_str());
-
-    let _pr = client
-        .pulls(&github.owner, &github.repo)
-        .create("chore: release", release_branch, default_branch)
-        .body("release-plz automatic bot")
-        .send()
-        .await?;
-
-    Ok(())
-}
-
-fn create_release_branch(repository: &Repo, release_branch: &str) -> anyhow::Result<()> {
-    repository.checkout_new_branch(release_branch)?;
-    repository.add_all_and_commit("chore: release")?;
-    repository.push(release_branch)?;
-    Ok(())
 }
---
+++ crates/release_plz_core/src/release_pr.rs
@@ -0,0 +1,77 @@
+use git_cmd::Repo;
+
+use anyhow::Context;
+use fake::Fake;
+use octocrab::OctocrabBuilder;
+use secrecy::{ExposeSecret, SecretString};
+use tracing::{instrument, Span};
+
+use crate::{next_versions, UpdateRequest};
+
+#[derive(Debug)]
+pub struct ReleasePrRequest {
+    pub github: GitHub,
+    pub update_request: UpdateRequest,
+}
+
+#[derive(Debug)]
+pub struct GitHub {
+    pub owner: String,
+    pub repo: String,
+    pub token: SecretString,
+}
+
+/// Update a local rust project and raise a pull request
+#[instrument]
+pub async fn release_pr(input: &ReleasePrRequest) -> anyhow::Result<()> {
+    let (packages_to_update, repository) = next_versions(&input.update_request)?;
+    if !packages_to_update.is_empty() {
+        // TODO think about better naming
+        let random_number: u64 = (100_000_000..999_999_999).fake();
+        let release_branch = format!("release-{}", random_number);
+        create_release_branch(&repository, &release_branch)?;
+        open_pr(&release_branch, &input.github).await?;
+    }
+
+    Ok(())
+}
+
+#[instrument(
+    fields(
+        default_branch = tracing::field::Empty,
+    )
+)]
+async fn open_pr(release_branch: &str, github: &GitHub) -> anyhow::Result<()> {
+    let client = OctocrabBuilder::new()
+        .personal_token(github.token.expose_secret().clone())
+        .build()
+        .context("Failed to build GitHub client")?;
+
+    let default_branch = client
+        .repos(&github.owner, &github.repo)
+        .get()
+        .await
+        .context(format!(
+            "failed to retrieve GitHub repository {}/{}",
+            github.owner, github.repo
+        ))?
+        .default_branch
+        .context("failed to retrieve default branch")?;
+    Span::current().record("default_branch", &default_branch.as_str());
+
+    let _pr = client
+        .pulls(&github.owner, &github.repo)
+        .create("chore: release", release_branch, default_branch)
+        .body("release-plz automatic bot")
+        .send()
+        .await?;
+
+    Ok(())
+}
+
+fn create_release_branch(repository: &Repo, release_branch: &str) -> anyhow::Result<()> {
+    repository.checkout_new_branch(release_branch)?;
+    repository.add_all_and_commit("chore: release")?;
+    repository.push(release_branch)?;
+    Ok(())
+}
