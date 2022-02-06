ec88fdd949edd7dea8fdcdffb25bb403d8a3cdf7
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-02-06 17:20:06 +0100

    improve logs

--- crates/release_plz/src/log.rs
+++ crates/release_plz/src/log.rs
@@ -1,10 +1,15 @@
-use tracing::Level;
 use tracing_log::LogTracer;
-use tracing_subscriber::FmtSubscriber;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
 
+/// Use `info` level by default, but you can customize it with `RUST_LOG` environment variable.
 pub fn init() {
+    let env_filter = tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+
     let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::INFO)
+        .with_env_filter(env_filter)
+        .with_line_number(true)
+        .pretty()
         .finish();
 
     tracing::subscriber::set_global_default(subscriber)
