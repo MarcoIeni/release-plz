094663173cfc50891dee9cea439c4a4b2360ebb3
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-01-09 22:27:50 +0100

    add test

--- crates/next_version/tests/all/pre_release.rs
+++ crates/next_version/tests/all/pre_release.rs
@@ -32,3 +32,11 @@ fn dot_1_is_added_to_last_identifier_in_pre_release() {
     let expected = Version::parse("1.0.0-beta.1.3").unwrap();
     assert_eq!(version.next(commits), expected);
 }
+
+#[test]
+fn dot_1_is_added_to_character_identifier_in_pre_release() {
+    let commits = vec!["feat!: break user"];
+    let version = Version::parse("1.0.0-beta.1.a").unwrap();
+    let expected = Version::parse("1.0.0-beta.1.a.1").unwrap();
+    assert_eq!(version.next(commits), expected);
+}
