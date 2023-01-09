952f33a2a5fb680ece48fbb26f6fc2af33706fce
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-01-09 22:14:27 +0100

    add tests

--- crates/next_version/tests/all/pre_release.rs
+++ crates/next_version/tests/all/pre_release.rs
@@ -8,3 +8,27 @@ fn commit_without_semver_prefix_increments_pre_release_version() {
     let expected = Version::parse("1.0.0-alpha.3").unwrap();
     assert_eq!(version.next(commits), expected);
 }
+
+#[test]
+fn commit_with_breaking_change_increments_pre_release_version() {
+    let commits = vec!["feat!: break user"];
+    let version = Version::parse("1.0.0-alpha.2").unwrap();
+    let expected = Version::parse("1.0.0-alpha.3").unwrap();
+    assert_eq!(version.next(commits), expected);
+}
+
+#[test]
+fn dot_1_is_added_to_unversioned_pre_release() {
+    let commits = vec!["feat!: break user"];
+    let version = Version::parse("1.0.0-alpha").unwrap();
+    let expected = Version::parse("1.0.0-alpha.1").unwrap();
+    assert_eq!(version.next(commits), expected);
+}
+
+#[test]
+fn dot_1_is_added_to_last_identifier_in_pre_release() {
+    let commits = vec!["feat!: break user"];
+    let version = Version::parse("1.0.0-beta.1.2").unwrap();
+    let expected = Version::parse("1.0.0-beta.1.3").unwrap();
+    assert_eq!(version.next(commits), expected);
+}
