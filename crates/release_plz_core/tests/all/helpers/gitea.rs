ccef94c513d2a6a5208b312f51bdbbf402d32a21
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-01-12 22:34:16 +0100

    create user

--- crates/release_plz_core/tests/all/helpers/gitea.rs
+++ crates/release_plz_core/tests/all/helpers/gitea.rs
@@ -1,11 +1,48 @@
-pub struct CreateUserOption
-/// Create a user and return it's token.
-fn create_user() -> String {
-        let client = reqwest::Client::new();
-        client.post(format!("{}/admin/users", base_url()))
-            .json(json
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, Default, Serialize)]
+pub struct CreateUserOption<'a> {
+    pub email: &'a str,
+    pub password: &'a str,
+    pub username: &'a str,
 }
 
-const fn base_url() -> String {
-        "http://localhost:3000/api/v1".to_string()
+#[derive(Clone, Debug, PartialEq, Default, Deserialize)]
+struct TokenResponse {
+    sha1: String,
+}
+
+/// Create a user and return it's token.
+async fn create_user() -> String {
+    let client = reqwest::Client::new();
+    let username = "me";
+    let admin_pwd: Option<String> = None;
+    let user_pwd = "password";
+    client
+        .post(format!("{}/admin/users", base_url()))
+        .basic_auth("root", admin_pwd.clone())
+        .json(&CreateUserOption {
+            email: "me@example.com",
+            password: user_pwd,
+            username,
+        })
+        .send()
+        .await
+        .unwrap();
+
+    let token: TokenResponse = client
+        .post(format!("{}/users/{username}/tokens", base_url()))
+        .basic_auth(username, Some(user_pwd))
+        .send()
+        .await
+        .unwrap()
+        .json()
+        .await
+        .unwrap();
+
+    token.sha1
+}
+
+fn base_url() -> String {
+    "http://localhost:3000/api/v1".to_string()
 }
