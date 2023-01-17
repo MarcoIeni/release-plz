54e1d3f03099034340e2fb706695cd7376cd0a0c
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-01-17 23:19:46 +0100

    user agent

--- crates/release_plz_core/src/github_client.rs
+++ crates/release_plz_core/src/github_client.rs
@@ -48,6 +48,7 @@ impl<'a> GitHubClient<'a> {
         let headers = default_headers(&github.token)?;
 
         let client = reqwest::Client::builder()
+            .user_agent("release-plz")
             .default_headers(headers)
             .build()
             .context("can't build GitHub client")?;
