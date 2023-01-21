de7b8f9d671c7cb528f79daf3dbb494d9b0b4fe2
era <me@elias.sh>
2023-01-21 16:52:43 +0000

    remove helper struct in favour of the one the crate already has

--- crates/release_plz_core/tests/all/gitea_client.rs
+++ crates/release_plz_core/tests/all/gitea_client.rs
@@ -1,27 +1,12 @@
 use crate::helpers::gitea;
 use release_plz_core::{Gitea, RepoUrl};
+use secrecy::ExposeSecret;
 
-struct User {
-    username: String,
-    token: String,
-    repo_name: String,
-}
-
-impl User {
-    fn to_gitea(&self) -> Gitea {
-        let url: String = format!("{}/{}/{}", gitea::base_url(), self.username, self.repo_name);
-        Gitea::new(RepoUrl::new(&url).unwrap(), self.token.clone().into()).unwrap()
-    }
-}
-
-async fn setup(username: String, repo_name: String) -> User {
+async fn setup(username: String, repo_name: String) -> Gitea {
     let token = gitea::create_user(&username).await;
     gitea::create_repo(&token, &repo_name).await;
-    User {
-        username,
-        token,
-        repo_name,
-    }
+    let url: String = format!("{}/{}/{}", gitea::base_url(), username, repo_name);
+    Gitea::new(RepoUrl::new(&url).unwrap(), token.clone().into()).unwrap()
 }
 
 #[tokio::test]
@@ -29,7 +14,13 @@ async fn gitea_client_creates_pr() {
     //TODO testing src/gitea_client.rs
     let user = setup("me".into(), "test_repo".into()).await;
     let branch_for_pr = "test_branch";
-    gitea::create_branch(&user.token, &user.repo_name, &user.username, branch_for_pr).await;
+    gitea::create_branch(
+        user.token.expose_secret(),
+        &user.repo,
+        &user.owner,
+        branch_for_pr,
+    )
+    .await;
 
     // TODO create pr
     // TODO check if pr exists
