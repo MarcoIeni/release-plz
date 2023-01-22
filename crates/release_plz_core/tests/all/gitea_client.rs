f58c9de76810050bde8d05eb6d0be5047d1bfdd2
era <me@elias.sh>
2023-01-22 09:42:23 +0000

    creating repo and release MR

--- crates/release_plz_core/tests/all/gitea_client.rs
+++ crates/release_plz_core/tests/all/gitea_client.rs
@@ -1,27 +1,84 @@
 use crate::helpers::gitea;
-use release_plz_core::{Gitea, RepoUrl};
+use anyhow::Context;
+use chrono::NaiveDate;
+use git_cmd::git_in_dir;
+use release_plz_core::CARGO_TOML;
+use release_plz_core::{
+    ChangelogRequest, GitBackend, Gitea, ReleasePrRequest, RepoUrl, UpdateRequest,
+};
 use secrecy::ExposeSecret;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+use tempfile::tempdir;
 
-async fn setup(username: String, repo_name: String) -> Gitea {
+const TEST_REPO: &str = "test_repo";
+const USERNAME: &str = "me";
+
+async fn setup(project_dir: &PathBuf, username: String, repo_name: String) -> Gitea {
     let token = gitea::create_user(&username).await;
     gitea::create_repo(&token, &repo_name).await;
-    let url: String = format!("{}/{}/{}", gitea::base_url(), username, repo_name);
-    Gitea::new(RepoUrl::new(&url).unwrap(), token.clone().into()).unwrap()
+
+    //TODO move domain from here
+    let git_url: String = format!(
+        "http://{username}:{}@localhost:3000/{username}/{repo_name}",
+        gitea::DEFAULT_PASSWORD
+    );
+    init_repo(project_dir, &git_url);
+
+    let url_repo: String = format!("{}/{}/{}", gitea::base_url(), username, repo_name);
+    Gitea::new(RepoUrl::new(&url_repo).unwrap(), token.clone().into()).unwrap()
+}
+
+fn init_repo(project_dir: &PathBuf, git_url: &str) {
+    Command::new("git")
+        .arg("clone")
+        .arg(git_url)
+        .arg(project_dir.as_path().to_str().unwrap())
+        .output()
+        .unwrap();
+
+    Command::new("cargo")
+        .arg("init")
+        .arg(project_dir.as_path().to_str().unwrap())
+        .output()
+        .unwrap();
+
+    git_in_dir(project_dir.as_ref(), &["add", "."]).unwrap();
+    git_in_dir(project_dir.as_ref(), &["commit", "-m", "add README"]).unwrap();
 }
 
 #[tokio::test]
 async fn gitea_client_creates_pr() {
-    //TODO testing src/gitea_client.rs
-    let user = setup("me".into(), "test_repo".into()).await;
-    let branch_for_pr = "test_branch";
-    gitea::create_branch(
-        user.token.expose_secret(),
-        &user.repo,
-        &user.owner,
-        branch_for_pr,
-    )
-    .await;
+    let local_project_dir = tempdir().unwrap();
+    let local_project = local_project_dir.as_ref().join(TEST_REPO);
 
-    // TODO create pr
-    // TODO check if pr exists
+    let user = setup(&local_project, USERNAME.into(), TEST_REPO.into()).await;
+
+    let release_pr_request = gitea_release_pr_request(user, local_project.as_ref()).unwrap();
+
+    release_plz_core::release_pr(&release_pr_request)
+        .await
+        .context("could not release PR")
+        .unwrap();
+
+    //TODO check if PR was released
+}
+
+fn gitea_release_pr_request(user: Gitea, project: &Path) -> anyhow::Result<ReleasePrRequest> {
+    let git = GitBackend::Gitea(user);
+    Ok(ReleasePrRequest {
+        git,
+        update_request: update_request(project),
+    })
+}
+
+fn update_request(project: &Path) -> UpdateRequest {
+    UpdateRequest::new(project.join(CARGO_TOML))
+        .unwrap()
+        .with_changelog(ChangelogRequest {
+            release_date: NaiveDate::from_ymd_opt(2015, 5, 15),
+            changelog_config: None,
+        })
+        .with_registry_project_manifest(project.join(CARGO_TOML))
+        .unwrap()
 }
--- crates/release_plz_core/tests/all/helpers/gitea.rs
+++ crates/release_plz_core/tests/all/helpers/gitea.rs
@@ -1,6 +1,8 @@
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 
+pub const DEFAULT_PASSWORD: &str = "password";
+
 #[derive(Clone, Debug, PartialEq, Default, Serialize)]
 pub struct CreateUserOption<'a> {
     pub email: &'a str,
@@ -38,7 +40,6 @@ struct CreateBranchRequest<'a> {
 /// Create a user and return it's token.
 pub async fn create_user(username: &str) -> String {
     let client = reqwest::Client::new();
-    let user_pwd = "password";
 
     // using the sign form and not the api
     let response = client
@@ -46,8 +47,8 @@ pub async fn create_user(username: &str) -> String {
         .header("cookie", "lang=en-US; i_like_gitea=8e2779a79e7d3e28; _csrf=uBwdvQ2EKSS69kVzPIGOPI1OmoU6MTU5NDMxMTk2NzA1ODIxMjgzNw")
         .form(&CreateUserOption {
             email: "me@example.com",
-            password: user_pwd,
-            retype: user_pwd,
+            password: DEFAULT_PASSWORD,
+            retype: DEFAULT_PASSWORD,
             user_name: username,
         })
         .send()
@@ -56,13 +57,12 @@ pub async fn create_user(username: &str) -> String {
         .text()
         .await
         .unwrap();
-    dbg!(response);
 
     // this must be called with username + password instead of a token
     // since there is no token created
     let response = client
         .post(format!("{}/users/{username}/tokens", base_api_url()))
-        .basic_auth(username, Some(user_pwd))
+        .basic_auth(username, Some(DEFAULT_PASSWORD))
         .json(&TokenRequest { name: "test" })
         .send()
         .await
