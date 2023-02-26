610bb61ba3b0a884c663ac3e79fb9fef8f0b830a
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-02-26 16:07:05 +0100

    wip

--- crates/release_plz_core/src/update.rs
+++ crates/release_plz_core/src/update.rs
@@ -46,7 +46,7 @@ impl PackagesUpdate {
             .map(|(package, update)| match &update.semver_check {
                 SemverCheck::Incompatible(incomp) => {
                     format!(
-                        "\n### ⚠️ {} breaking changes\n\n```{}```\n",
+                        "\n### ⚠️ `{}` breaking changes\n\n```{}```\n",
                         package.name, incomp
                     )
                 }
