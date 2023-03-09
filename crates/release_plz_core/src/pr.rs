59f36f0f978c252030c2d649c68fdc2f14fd6892
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-03-09 06:15:13 +0000

    feat: include version in pr title for single crate (#593)

--- crates/release_plz_core/src/pr.rs
+++ crates/release_plz_core/src/pr.rs
@@ -40,11 +40,15 @@ fn pr_title(
     packages_to_update: &PackagesUpdate,
     project_contains_multiple_pub_packages: bool,
 ) -> String {
-    if packages_to_update.updates.len() > 1 || !project_contains_multiple_pub_packages {
+    if packages_to_update.updates.len() > 1 {
         "chore: release".to_string()
     } else {
         let (package, update) = &packages_to_update.updates[0];
-        format!("chore({}): release v{}", package.name, update.version)
+        if project_contains_multiple_pub_packages {
+            format!("chore({}): release v{}", package.name, update.version)
+        } else {
+            format!("chore: release v{}", update.version)
+        }
     }
 }
 
