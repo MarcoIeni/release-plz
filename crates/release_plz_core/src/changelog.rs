f48a9e5807f96274be778e2abdad99c2f14b11ed
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-04-29 16:02:04 +0200

    chore: update git-cliff

--- crates/release_plz_core/src/changelog.rs
+++ crates/release_plz_core/src/changelog.rs
@@ -1,6 +1,6 @@
 use anyhow::Context;
 use chrono::{DateTime, NaiveDate, Utc};
-use git_cliff::changelog::Changelog as GitCliffChangelog;
+use git_cliff_core::changelog::Changelog as GitCliffChangelog;
 use git_cliff_core::{
     commit::Commit,
     config::{ChangelogConfig, CommitParser, Config, GitConfig},
