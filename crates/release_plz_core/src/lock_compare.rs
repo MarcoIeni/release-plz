455b90c6dd03b0d8d8860d03eeca630b693c91c3
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-05-24 20:13:31 +0200

    perf: parse cargo lock faster (#795)

--- crates/release_plz_core/src/lock_compare.rs
+++ crates/release_plz_core/src/lock_compare.rs
@@ -1,7 +1,7 @@
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 
 use anyhow::Context;
-use cargo_lock::Lockfile;
+use serde::Deserialize;
 use tracing::debug;
 
 /// Compare the dependencies present in the `Cargo.lock` of the registry package and the local one.
@@ -21,40 +21,88 @@ pub fn are_lock_dependencies_updated(
 }
 
 fn are_dependencies_updated(local_lock: &Path, registry_lock: &Path) -> anyhow::Result<bool> {
-    let local_lock = Lockfile::load(local_lock)
+    let local_lock: Lockfile = read_lockfile(local_lock)
         .with_context(|| format!("failed to load lockfile of local package {:?}", local_lock))?;
-    let registry_lock = Lockfile::load(registry_lock).with_context(|| {
+    let registry_lock = read_lockfile(registry_lock).with_context(|| {
         format!(
             "failed to load lockfile of registry package {:?}",
             registry_lock
         )
     })?;
+    let registry_lock_packages = PackagesByName::new(&registry_lock.packages);
     Ok(are_dependencies_of_lockfiles_updated(
         &local_lock,
-        &registry_lock,
+        &registry_lock_packages,
     ))
 }
 
-fn are_dependencies_of_lockfiles_updated(local_lock: &Lockfile, registry_lock: &Lockfile) -> bool {
-    for local_package in &local_lock.packages {
-        // Cargo.lock can contain multiple packages with the same name but different versions.
-        let registry_packages: Vec<&cargo_lock::Package> = registry_lock
-            .packages
-            .iter()
-            .filter(|p| p.name == local_package.name)
-            .collect();
+fn read_lockfile(path: &Path) -> anyhow::Result<Lockfile> {
+    let content =
+        std::fs::read_to_string(path).with_context(|| format!("can't read lockfile {path:?}"))?;
+    let lockfile =
+        toml::from_str(&content).with_context(|| format!("invalid format of lockfile {path:?}"))?;
+    Ok(lockfile)
+}
 
-        let is_same_version = registry_packages.is_empty()
-            || registry_packages
+fn are_dependencies_of_lockfiles_updated(
+    local_lock: &Lockfile,
+    registry_lock: &PackagesByName,
+) -> bool {
+    for local_package in &local_lock.packages {
+        if let Some(registry_packages) = registry_lock.get(&local_package.name) {
+            let is_same_version = registry_packages
                 .iter()
                 .any(|p| p.version == local_package.version);
-        if !is_same_version {
-            debug!(
-                "Version of package {} changed to version {:?}",
-                local_package.name, local_package.version
-            );
-            return true;
+            if !is_same_version {
+                debug!(
+                    "Version of package {} changed to version {:?}",
+                    local_package.name, local_package.version
+                );
+                return true;
+            }
         }
     }
     false
+}
+
+#[derive(Deserialize, Debug)]
+struct Lockfile {
+    #[serde(rename = "package")]
+    packages: Vec<Package>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Package {
+    name: String,
+    version: String,
+}
+
+/// Packages grouped by name, to search faster.
+/// Cargo.lock can contain multiple packages with the same name but different versions.
+struct PackagesByName<'a> {
+    packages: HashMap<&'a str, Vec<&'a Package>>,
+}
+
+impl<'a> PackagesByName<'a> {
+    fn new(packages: &'a [Package]) -> Self {
+        let mut packages_by_name = HashMap::new();
+        for package in packages {
+            packages_by_name
+                .entry(package.name.as_str())
+                .or_insert_with(Vec::new)
+                .push(package);
+        }
+        Self {
+            packages: packages_by_name,
+        }
+    }
+
+    /// Get the packages with the given name.
+    fn get(&self, name: &str) -> Option<&[&Package]> {
+        self.packages.get(name).map(|p| {
+            // If the entry exists, it contains at least one package.
+            assert!(!p.is_empty());
+            p.as_slice()
+        })
+    }
 }
