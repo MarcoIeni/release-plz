ac027dff934b4f53e22a5fc7d1a8dc6c3c2c1e72
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-06-17 17:01:42 +0200

    create subdir

--- crates/release_plz_core/src/copy_dir.rs
+++ crates/release_plz_core/src/copy_dir.rs
@@ -6,14 +6,20 @@ use walkdir::WalkDir;
 #[test]
 fn is_dir_copied_correctly() {
     let temp = tempfile::tempdir().unwrap();
-    let file1 = temp.path().join("file1");
+    let subdir = "subdir";
+    let subdir_path = temp.path().join(subdir);
+    fs::create_dir(&subdir_path).unwrap();
+
+    let file1 = subdir_path.join("file1");
     std::fs::write(&file1, "aaa").unwrap();
-    let file2 = temp.path().join("file2");
+    let file2 = subdir_path.join("file2");
     create_symlink(&file1, file2).unwrap();
 
     let temp2 = tempfile::tempdir().unwrap();
-    dir_copy(temp.path(), temp2.path()).unwrap();
-    let new_file2 = temp2.path().join("file2");
+    println!("from: {:?} to temp2: {:?}", subdir_path, temp2.path());
+    dir_copy(subdir_path, temp2.path()).unwrap();
+    let temp2_subdir = temp2.path().join(subdir);
+    let new_file2 = temp2_subdir.join("file2");
     assert!(fs::symlink_metadata(new_file2).unwrap().is_symlink());
 }
 
@@ -43,17 +49,23 @@ fn create_symlink<P: AsRef<Path>, Q: AsRef<Path>>(original: P, link: Q) -> io::R
 /// `to` is created if it doesn't exist.
 pub fn dir_copy<P: AsRef<Path>, Q: AsRef<Path>>(from: P, to: Q) -> anyhow::Result<()> {
     let from = from.as_ref();
-    let to = to.as_ref();
+    let dir_name = from
+        .components()
+        .last()
+        .context("invalid path")?
+        .as_os_str();
+    anyhow::ensure!(from.is_dir(), "not a directory: {:?}", from);
+    let to = to.as_ref().join(dir_name);
     println!("from: {:?}", from);
     println!("to: {:?}", to);
     if !to.exists() {
-        fs::create_dir_all(to).context("cannot create directory")?;
+        fs::create_dir_all(&to).context("cannot create directory")?;
     }
     for entry in WalkDir::new(from) {
         println!("entry: {:?}", entry);
         let entry = entry.context("invalid entry")?;
         let file_type = entry.file_type();
-        let mut dest_path = to.to_owned();
+        let mut dest_path = to.clone();
         let relative = entry.path().strip_prefix(from).unwrap();
         dest_path.push(relative);
 
@@ -61,15 +73,19 @@ pub fn dir_copy<P: AsRef<Path>, Q: AsRef<Path>>(from: P, to: Q) -> anyhow::Resul
             if dest_path == to {
                 continue;
             }
-            fs::create_dir(&dest_path)?;
+            fs::create_dir(&dest_path)
+                .with_context(|| format!("cannot create directory {dest_path:?}"))?;
         } else if file_type.is_symlink() {
             // TODO: can we create symlink if new path exists?
-            let original_link = fs::read_link(entry.path())?;
+            let original_link = fs::read_link(entry.path())
+                .with_context(|| format!("cannot read link {:?}", entry.path()))?;
             let new_relative = original_link.strip_prefix(from)?;
             let new_link = to.join(new_relative);
-            create_symlink(new_link, &dest_path)?;
+            create_symlink(new_link, &dest_path)
+                .with_context(|| format!("cannot create symlink {:?}", dest_path))?;
         } else if file_type.is_file() {
-            fs::copy(entry.path(), &dest_path)?;
+            fs::copy(entry.path(), &dest_path)
+                .with_context(|| format!("cannot copy file {:?}", entry.path()))?;
         }
     }
 
