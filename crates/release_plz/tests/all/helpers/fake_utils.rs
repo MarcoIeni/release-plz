848af24b8a45878f55ffbdeb55dbd2125a1d5d63
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-08-17 17:21:09 +0200

    wip

--- crates/release_plz/tests/all/helpers/fake_utils.rs
+++ crates/release_plz/tests/all/helpers/fake_utils.rs
@@ -1,7 +1,7 @@
 use fake::{Fake, StringFaker};
 
 pub fn fake_id() -> String {
-    const LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const LETTERS: &str = "abcdefghijklmnopqrstuvwxyz";
     let f = StringFaker::with(Vec::from(LETTERS), 8);
     f.fake()
 }
