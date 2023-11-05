8bae77c5df9418e5663f2e0932d0e3e028a75aae
Rob Ede <robjtede@icloud.com>
2023-11-05 15:06:55 +0000

    feat: support .release-plz.toml as a config file (#1057)

--- crates/release_plz/src/args/mod.rs
+++ crates/release_plz/src/args/mod.rs
@@ -57,20 +57,52 @@ fn local_manifest(project_manifest: Option<&Path>) -> PathBuf {
 }
 
 fn parse_config(config_path: Option<&Path>) -> anyhow::Result<Config> {
-    let config_path: PathBuf = config_path
-        .map(|p| p.to_path_buf())
-        .unwrap_or("release-plz.toml".into());
-    match std::fs::read_to_string(&config_path) {
-        Ok(config) => {
-            info!("using release-plz config file {}", config_path.display());
-            toml::from_str(&config).with_context(|| format!("invalid config file {config_path:?}"))
+    let (config, path) = if let Some(config_path) = config_path {
+        match std::fs::read_to_string(config_path) {
+            Ok(config) => (config, config_path),
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::NotFound => {
+                    anyhow::bail!("specified config does not exist at path {config_path:?}")
+                }
+                _ => anyhow::bail!("can't read {config_path:?}: {e:?}"),
+            },
         }
-        Err(e) => match e.kind() {
-            std::io::ErrorKind::NotFound => {
+    } else {
+        match first_file_contents([
+            Path::new("release-plz.toml"),
+            Path::new(".release-plz.toml"),
+        ])? {
+            Some((config, path)) => (config, path),
+            None => {
                 info!("release-plz config file not found, using default configuration");
-                Ok(Config::default())
+                return Ok(Config::default());
             }
-            _ => anyhow::bail!("can't read {config_path:?}: {e:?}"),
-        },
+        }
+    };
+
+    info!("using release-plz config file {}", path.display());
+    toml::from_str(&config).with_context(|| format!("invalid config file {config_path:?}"))
+}
+
+/// Returns the contents of the first file that exists.
+///
+/// If none of the files exist, returns `Ok(None)`.
+///
+/// # Errors
+///
+/// Errors if opening and reading one of files paths fails for reasons other that it doesn't exist.
+fn first_file_contents<'a>(
+    paths: impl IntoIterator<Item = &'a Path>,
+) -> anyhow::Result<Option<(String, &'a Path)>> {
+    let paths = paths.into_iter();
+
+    for path in paths {
+        match std::fs::read_to_string(path) {
+            Ok(config) => return Ok(Some((config, path))),
+            Err(err) if err.kind() == std::io::ErrorKind::NotFound => {}
+            Err(err) => return Err(err.into()),
+        }
     }
+
+    Ok(None)
 }
