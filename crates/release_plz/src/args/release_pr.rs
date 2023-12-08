3003989194715e8d66cbd529214191971e77ce12
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-12-08 22:58:06 +0100

    chore(args): hide the environment value of git token (#1124)
    Co-authored-by: Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>

--- crates/release_plz/src/args/release_pr.rs
+++ crates/release_plz/src/args/release_pr.rs
@@ -13,7 +13,7 @@ pub struct ReleasePr {
     #[command(flatten)]
     pub update: Update,
     /// Git token used to create the pull request.
-    #[arg(long, value_parser = NonEmptyStringValueParser::new(), visible_alias = "github-token", env)]
+    #[arg(long, value_parser = NonEmptyStringValueParser::new(), visible_alias = "github-token", env, hide_env_values=true)]
     git_token: String,
     /// Kind of git host where your project is hosted.
     #[arg(long, value_enum, default_value_t = GitBackendKind::Github)]
