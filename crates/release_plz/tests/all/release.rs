e132de422dd2e0dc6ee5115278f782add1d80f8c
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2024-02-11 18:09:12 +0100

    integration test

--- crates/release_plz/tests/all/release.rs
+++ crates/release_plz/tests/all/release.rs
@@ -22,3 +22,25 @@ async fn release_plz_releases_a_new_project_with_custom_tag_name() {
 
     assert!(is_tag_created());
 }
+
+#[tokio::test]
+#[cfg_attr(not(feature = "docker-tests"), ignore)]
+async fn release_plz_releases_a_new_project_with_custom_release_name() {
+    let context = TestContext::new().await;
+
+    let config = r#"
+    [workspace]
+    git_release_name = "{{ package}}--{{ version }}"
+    "#;
+    context.write_release_plz_toml(config);
+
+    let crate_name = &context.gitea.repo;
+
+    let expected_tag = "v0.1.0";
+    let expected_release = format!("{}--0.1.0", crate_name);
+
+    context.run_release().success();
+
+    let gitea_release = context.gitea.get_gitea_release(expected_tag).await;
+    assert_eq!(gitea_release.name, expected_release);
+}
