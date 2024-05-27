44ebabb9b98887ff3928ded08ff7e8f0867e3c6d
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2024-05-27 22:17:16 +0200

    fix: readme comparison

---
+++ crates/release_plz/tests/all/release_pr.rs
@@ -0,0 +1,47 @@
+use cargo_utils::LocalManifest;
+
+use crate::helpers::test_context::TestContext;
+
+#[tokio::test]
+#[cfg_attr(not(feature = "docker-tests"), ignore)]
+async fn release_plz_detects_edited_readme_cargo_toml_field() {
+    let context = TestContext::new().await;
+
+    context.run_release_pr().success();
+    context.merge_release_pr().await;
+
+    let expected_tag = "v0.1.0";
+
+    context.run_release().success();
+
+    let gitea_release = context.gitea.get_gitea_release(expected_tag).await;
+    assert_eq!(gitea_release.name, expected_tag);
+
+    let readme = "README.md";
+    let new_readme = format!("NEW_{readme}");
+    let old_readme_path = context.repo_dir().join(readme);
+    let new_readme_path = context.repo_dir().join(&new_readme);
+    fs_err::rename(&old_readme_path, &new_readme_path).unwrap();
+
+    let cargo_toml_path = context.repo_dir().join("Cargo.toml");
+    let mut cargo_toml = LocalManifest::try_new(&cargo_toml_path).unwrap();
+    cargo_toml.data["package"]["readme"] = toml_edit::value(new_readme);
+    cargo_toml.write().unwrap();
+
+    context.repo.add_all_and_commit("add new readme").unwrap();
+    context.repo.git(&["push"]).unwrap();
+
+    context.run_release_pr().success();
+    context.merge_release_pr().await;
+
+    let expected_tag = "v0.1.1";
+
+    context.run_release().success();
+
+    let gitea_release = context.gitea.get_gitea_release(expected_tag).await;
+    assert_eq!(gitea_release.name, expected_tag);
+    expect_test::expect![[r#"
+        ### Other
+        - add new readme"#]]
+    .assert_eq(&gitea_release.body);
+}
