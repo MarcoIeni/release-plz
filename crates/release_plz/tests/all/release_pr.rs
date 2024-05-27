b1a58aa7b1310d751933e8c3fe6c3e20bd3fd86b
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2024-05-27 22:44:16 +0200

    wip

--- crates/release_plz/tests/all/release_pr.rs
+++ crates/release_plz/tests/all/release_pr.rs
@@ -17,19 +17,7 @@ async fn release_plz_detects_edited_readme_cargo_toml_field() {
     let gitea_release = context.gitea.get_gitea_release(expected_tag).await;
     assert_eq!(gitea_release.name, expected_tag);
 
-    let readme = "README.md";
-    let new_readme = format!("NEW_{readme}");
-    let old_readme_path = context.repo_dir().join(readme);
-    let new_readme_path = context.repo_dir().join(&new_readme);
-    fs_err::rename(&old_readme_path, &new_readme_path).unwrap();
-
-    let cargo_toml_path = context.repo_dir().join("Cargo.toml");
-    let mut cargo_toml = LocalManifest::try_new(&cargo_toml_path).unwrap();
-    cargo_toml.data["package"]["readme"] = toml_edit::value(new_readme);
-    cargo_toml.write().unwrap();
-
-    context.repo.add_all_and_commit("add new readme").unwrap();
-    context.repo.git(&["push"]).unwrap();
+    move_readme(&context);
 
     context.run_release_pr().success();
     context.merge_release_pr().await;
@@ -44,4 +32,20 @@ async fn release_plz_detects_edited_readme_cargo_toml_field() {
         ### Other
         - add new readme"#]]
     .assert_eq(&gitea_release.body);
+}
+
+fn move_readme(context: &TestContext) {
+    let readme = "README.md";
+    let new_readme = format!("NEW_{readme}");
+    let old_readme_path = context.repo_dir().join(readme);
+    let new_readme_path = context.repo_dir().join(&new_readme);
+    fs_err::rename(old_readme_path, new_readme_path).unwrap();
+
+    let cargo_toml_path = context.repo_dir().join("Cargo.toml");
+    let mut cargo_toml = LocalManifest::try_new(&cargo_toml_path).unwrap();
+    cargo_toml.data["package"]["readme"] = toml_edit::value(new_readme);
+    cargo_toml.write().unwrap();
+
+    context.repo.add_all_and_commit("add new readme").unwrap();
+    context.repo.git(&["push"]).unwrap();
 }
