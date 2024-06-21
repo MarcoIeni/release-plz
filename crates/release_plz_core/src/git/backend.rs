29701b242fa6f572067a5064a0bdf5b5256b513c
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-06-21 08:29:17 +0200

    more docs

--- crates/release_plz_core/src/git/backend.rs
+++ crates/release_plz_core/src/git/backend.rs
@@ -197,7 +197,17 @@ impl GitClient {
             .json(&create_release_options)
             .send()
             .await?
-            .error_for_status()?;
+            .error_for_status()
+            .map_err(|e| {
+                if let Some(status) = e.status() {
+                    if status == reqwest::StatusCode::FORBIDDEN {
+                        return anyhow::anyhow!(e).context(
+                            "Make sure your token has sufficient permissions. Learn more at https://release-plz.ieni.dev/docs/usage/release or https://release-plz.ieni.dev/docs/github/token",
+                        );
+                    }
+                }
+                anyhow::anyhow!(e)
+            })?;
         Ok(())
     }
 
