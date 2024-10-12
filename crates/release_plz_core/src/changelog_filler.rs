90d423b78ecf59845f689a91a0662d7f11bb2a12
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-10-12 09:56:14 +0200

    make api call only if needed

--- crates/release_plz_core/src/changelog_filler.rs
+++ crates/release_plz_core/src/changelog_filler.rs
@@ -12,7 +12,14 @@ pub struct RequiredInfo {
     author_email: bool,
     committer_name: bool,
     committer_email: bool,
-    remote: bool,
+    remote_username: bool,
+    remote_pr_number: bool,
+}
+
+impl RequiredInfo {
+    fn is_remote_required(&self) -> bool {
+        self.remote_username || self.remote_pr_number
+    }
 }
 
 pub async fn fill_commit<'a>(
@@ -39,15 +46,23 @@ pub async fn fill_commit<'a>(
         if required_info.committer_email {
             commit.committer.email = Some(repository.get_committer_email(&commit.id)?);
         }
-        if required_info.remote {
+        if required_info.is_remote_required() {
             let git_client = git_client
                 .context("The changelog template requires information from the remote, but git token wasn't provided")?;
-            let remote_commit = git_client.get_remote_commit(&commit.id).await?;
+            let username = if required_info.remote_username {
+                git_client.get_remote_commit(&commit.id).await?.username
+            } else {
+                None
+            };
+            let pr_number = if required_info.remote_pr_number {
+                let associated_prs = git_client.associated_prs(&commit.id).await?;
+                associated_prs.first().map(|pr| pr.number)
+            } else {
+                None
+            };
 
-            let associated_prs = git_client.associated_prs(&commit.id).await?;
-            let pr_number = associated_prs.first().map(|pr| pr.number);
             commit.remote = RemoteContributor {
-                username: remote_commit.username,
+                username,
                 pr_number: pr_number.and_then(|n| i64::try_from(n).ok()),
                 ..RemoteContributor::default()
             };
@@ -63,7 +78,8 @@ pub fn get_required_info(changelog_config: &ChangelogConfig) -> RequiredInfo {
         author_email: false,
         committer_name: false,
         committer_email: false,
-        remote: false,
+        remote_username: false,
+        remote_pr_number: false,
     };
 
     if let Some(body) = changelog_config.body.as_ref() {
@@ -71,7 +87,8 @@ pub fn get_required_info(changelog_config: &ChangelogConfig) -> RequiredInfo {
         required_info.author_email = body.contains("author.email");
         required_info.committer_name = body.contains("committer.name");
         required_info.committer_email = body.contains("committer.email");
-        required_info.remote = body.contains("commit.remote");
+        required_info.remote_username = body.contains("remote.username");
+        required_info.remote_pr_number = body.contains("remote.pr_number");
     }
 
     required_info
