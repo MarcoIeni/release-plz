6aa4e37bfe777676826c81c061d7240c6c05dcf4
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-10-16 09:36:44 +0000

    wip

--- crates/release_plz_core/src/command/release.rs
+++ crates/release_plz_core/src/command/release.rs
@@ -1,7 +1,7 @@
 use std::{collections::BTreeMap, time::Duration};
 
 use anyhow::Context;
-use cargo::{core::global_cache_tracker::GitCheckout, util::VersionExt};
+use cargo::util::VersionExt;
 use cargo_metadata::{
     camino::{Utf8Path, Utf8PathBuf},
     semver::Version,
