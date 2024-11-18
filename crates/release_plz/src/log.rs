ebe068bf34d0aa51cca8eb00e072c70550767e45
EleisonC <ckalule7@gmail.com>
2024-11-18 20:40:40 +0300

    add compact fmt to make logging less verbose

--- crates/release_plz/src/log.rs
+++ crates/release_plz/src/log.rs
@@ -6,9 +6,10 @@ pub fn init(verbose: bool) {
     let subscriber = {
         let env_filter = tracing_subscriber::EnvFilter::try_from_default_env()
             .unwrap_or_else(|_| EnvFilter::new("info"));
-        let mut event_fmt = tracing_subscriber::fmt::format().pretty();
-        if !verbose {
-            event_fmt = event_fmt.with_target(false).with_source_location(false);
+        let mut event_fmt = tracing_subscriber::fmt::format().compact();
+        
+        if verbose {
+            event_fmt = event_fmt.with_target(true).with_source_location(true);
         }
         FmtSubscriber::builder()
             .with_env_filter(env_filter)
