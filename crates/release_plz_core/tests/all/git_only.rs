d201cca1ae1fb16ce838af2be707173a2d55e994
Arav Singhal <singhal.arav@gmail.com>
2024-12-06 09:52:51 -0600

    fix(core): Test package excludes in git_only::single_crate

--- crates/release_plz_core/tests/all/git_only.rs
+++ crates/release_plz_core/tests/all/git_only.rs
@@ -1,5 +1,6 @@
 use crate::helpers::git_only_test::{GitOnlyTestContext, PROJECT_NAME};
 use cargo_metadata::semver::Version;
+use cargo_utils::{LocalManifest, CARGO_TOML};
 use release_plz_core::PackagesUpdate;
 use std::path::PathBuf;
 
@@ -47,6 +48,8 @@ async fn single_crate() {
         .expect("initial update should succeed")
         .assert_packages_updated([(PROJECT_NAME, Version::new(0, 1, 0), Version::new(0, 1, 0))]);
 
+    context.add_all_commit_and_push("chore: release");
+
     context
         .run_release()
         .await
@@ -56,11 +59,38 @@ async fn single_crate() {
     touch(context.project_dir().join("included")).unwrap();
     context.add_all_commit_and_push("fix: Add `included` file");
 
+    // Add package excludes
+    let mut cargo_toml = LocalManifest::try_new(&context.project_dir().join(CARGO_TOML)).unwrap();
+    const EXCLUDED_FILENAME: &'static str = "excluded";
+    cargo_toml.data["package"]["exclude"] =
+        toml_edit::Item::from(toml_edit::Array::from_iter([EXCLUDED_FILENAME]));
+    cargo_toml.write().unwrap();
+
+    context.add_all_commit_and_push("fix: Exclude `excluded` from package");
+
     context
         .run_update()
         .await
         .expect("second update should succeed")
         .assert_packages_updated([(PROJECT_NAME, Version::new(0, 1, 0), Version::new(0, 1, 1))]);
 
-    // TODO: Test package excludes
+    context.add_all_commit_and_push("chore: release");
+
+    context
+        .run_release()
+        .await
+        .expect("second release should succeed")
+        .expect("second release should not be empty");
+
+    touch(context.project_dir().join(EXCLUDED_FILENAME)).unwrap();
+    context.add_all_commit_and_push("chore: Add `excluded` file");
+
+    // Modifying file excluded from package should not lead to version increment
+    context
+        .run_update()
+        .await
+        .expect("update should succeed")
+        .assert_packages_updated([]);
+
+    assert!(context.repo.is_clean().is_ok());
 }
